5a36b9571d2000761053b17670f0c5d2564a527e
kalishavali <kalishavali.rgukt@gmail.com>
2016-12-02 13:53:42 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -7,16 +7,38 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		Scanner console=new Scanner(System.in);
 		String s=console.nextLine();
-		int n=console.nextInt();
+		int target=console.nextInt();
+		
 		String[] arr=s.split("\\,");
-		for(int i=0;i<arr.length;i++){
-			int target=Integer.parseInt(arr[i]);
-			if(target==n){
-				System.out.println("true\nposition = "+i);
-			}else{
-				System.out.println("false");
+		int[] nums=new int[arr.length];
+		for(int i=0;i<arr.length;i++)
+			nums[i]=Integer.parseInt(arr[i]);
+		
+		int position=-1;
+		for(int j=0;j<nums.length;j++){
+				if(nums[j]==target)
+					position=j;
+		}
+		
+		Arrays.sort(nums);
+		System.out.println(binarySearch(nums,target)+"\nposition = "+position);
+		
+	}
+	//O(N)
+	static boolean binarySearch(int[] numbers,int target){
+		int min=0;
+		int max=numbers.length-1;
+		while(min<=max){
+			int mid=(min+max)/2;
+			if(numbers[mid]<target){
+				min=mid+1;
+			}else if(numbers[mid]>target){
+				max=mid-1;
+			}else if(numbers[mid]==target){
+				return true;
 			}
 		}
+		return false;
 	}
 	//O(logN)
 }
