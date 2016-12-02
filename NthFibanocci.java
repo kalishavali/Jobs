1c617bac53d4d5307098c1f6a5806b8321adc448
kalishavali <kalishavali.rgukt@gmail.com>
2016-12-02 13:15:17 +0530

    Update NthFibanocci.java

--- NthFibanocci.java
+++ NthFibanocci.java
@@ -3,17 +3,18 @@ package Hackerrank;
 import java.util.Scanner;
 
 public class NthFibanocci {
-	  public static long fibonacci(int n) {
-	        if (n <= 1) return n;
-	        else return fibonacci(n-1) + fibonacci(n-2);
-	    }
-
-	    public static void main(String[] args) {
+	  public static void main(String[] args) {
 	        Scanner console=new Scanner(System.in);
 	        int n=console.nextInt();
-	        System.out.print(0);
-	        for (int i = 1; i <= n; i++)
-	            System.out.print(" "+fibonacci(i));
-	        System.out.println("\n"+fibonacci(n));
+	        int a=0;
+	        int b=1;
+	        System.out.print(a+" "+b);
+	        for (int i = 3; i <= n; i++){
+	        	int c=a+b;
+	        	a=b;
+	        	b=c;
+	        	System.out.print(" "+c);
+	        }
+	        System.out.print("\n"+b);
 	    }
 }
